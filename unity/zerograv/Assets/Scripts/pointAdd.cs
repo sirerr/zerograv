87e85e6d38193e153d6181f5a47fc5ae817b673a
sirerr <sirerr@gmail.com>
2016-10-08 17:41:07 -0400

    Leonard- still working on points

---
+++ unity/zerograv/Assets/Scripts/pointAdd.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class pointAdd : MonoBehaviour {
+
+	public int playerpoints =0;
+
+	public Text pointtext;
+
+
+	public void gainedPoint()
+	{
+
+		pointtext.text = playerpoints++.ToString();
+	}
+
+}
