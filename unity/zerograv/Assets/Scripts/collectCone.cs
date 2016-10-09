3648839677b1326b2ceab3f15eaca8c49000b91c
sirerr <sirerr@gmail.com>
2016-10-08 20:21:53 -0400

    Leonard- UI objects

---
+++ unity/zerograv/Assets/Scripts/collectCone.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class collectCone : MonoBehaviour {
+
+	public void OnTriggerEnter(Collider col)
+	{
+		GameObject obj = GameObject.FindGameObjectWithTag("pointcanvas");
+
+		obj.GetComponent<pointAdd>().gainedPoint();
+		Destroy(gameObject);
+	}
+
+}
--- unity/zerograv/Assets/Scripts/pointAdd.cs
+++ unity/zerograv/Assets/Scripts/pointAdd.cs
@@ -6,14 +6,22 @@ using UnityEngine.UI;
 public class pointAdd : MonoBehaviour {
 
 	public int playerpoints =0;
-
+	public Canvas canvasobj;
 	public Text pointtext;
 
 
 	public void gainedPoint()
 	{
-
+		canvasobj.enabled = true;
 		pointtext.text = playerpoints++.ToString();
+
+	}
+
+	IEnumerator disableCanvas()
+	{
+		yield return new WaitForSeconds(10);
+		canvasobj.enabled = false;
+
 	}
 
 }
