be5b7938f03239e7daf8d8efacbe2b3a3b4a211c
benttoenail <maxrose8841@gmail.com>
2016-10-08 23:56:53 -0400

    Beginning Platform Logic

---
+++ unity/zerograv/Assets/Scripts/MakePlatform.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MakePlatform : MonoBehaviour {
+
+	[SerializeField] GameObject platform;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+        if (col.gameObject.tag == "Ball")
+        {
+            Debug.Log("There was collision!");
+            Instantiate(platform, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        }
+	}
+}
