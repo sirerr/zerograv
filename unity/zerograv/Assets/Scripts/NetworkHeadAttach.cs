bceb97d69bd5e4312143bedc64f1b5b57fd79b07
Daniel Bryand <daniel.bryand@gmail.com>
2016-10-09 09:07:51 -0400

    MAY BREAK SOME SCENES!! Changed the SwimPaddle Script to allow for requiring two paddles, smoother performance, and power script.

---
+++ unity/zerograv/Assets/Scripts/NetworkHeadAttach.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.VR;
+using UnityEngine.Networking;
+
+public class NetworkHeadAttach : NetworkBehaviour {
+
+	bool followVr = false;
+
+	private Transform playArea;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (followVr && playArea != null) {
+			transform.position = InputTracking.GetLocalPosition (VRNode.Head);
+			transform.rotation = InputTracking.GetLocalRotation (VRNode.Head);
+		}
+	}
+
+	public override void OnStartLocalPlayer() {
+		followVr = true;
+		playArea = GameObject.Find ("[CameraRig]").transform;
+		transform.parent = playArea;
+	}
+}
--- unity/zerograv/Assets/Scripts/SwimPaddle.cs
+++ unity/zerograv/Assets/Scripts/SwimPaddle.cs
@@ -8,7 +8,23 @@ public class SwimPaddle : MonoBehaviour {
     public float torqueMult = 1;
 
     public Transform centerOfMass;
-    public Transform playArea;
+	public Rigidbody playArea;
+
+	public enum SwimButton {
+		Trigger,
+		Grip,
+		TrackPress,
+		Menu
+	}
+	public SwimButton swimButton = SwimButton.Trigger;
+
+	[Tooltip ("Optional: If this is filled with another SwimButton object, both need to be triggered to produce force.")]
+	public SwimPaddle partnerPaddle;
+
+	[HideInInspector]
+	public bool isSwimming = false;
+	[HideInInspector]
+	public bool isTriggered = false;
 
     SteamVR_Controller.Device device;
     SteamVR_TrackedObject trackedObj;
@@ -17,7 +33,8 @@ public class SwimPaddle : MonoBehaviour {
     private Vector3 controllerDistance;
     private Vector3 controllerTorque;
 
-    private Vector3 lastControllerPosition;
+	private Vector3 lastControllerPositionGolbal;
+	private Vector3 lastControllerPositionLocal;
 
     void Awake()
     {
@@ -45,24 +62,65 @@ public class SwimPaddle : MonoBehaviour {
             Debug.Log("Device not found");
         }
 
-        if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
-        {
+		ulong triggerButton = SteamVR_Controller.ButtonMask.Trigger;
 
-            // float triggerVal = Mathf.Pow(device.GetAxis(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger).x, 2f);
+		switch (swimButton) {
+		case SwimButton.Trigger:
+			triggerButton = SteamVR_Controller.ButtonMask.Trigger;
+			break;
+		case SwimButton.Grip:
+			triggerButton = SteamVR_Controller.ButtonMask.Grip;
+			break;
+		case SwimButton.TrackPress:
+			triggerButton = SteamVR_Controller.ButtonMask.Touchpad;
+			break;
+		case SwimButton.Menu:
+			triggerButton = SteamVR_Controller.ButtonMask.ApplicationMenu;
+			break;
+		}
 
-            //controllerForce = GetComponent<Rigidbody>().velocity * -forceMult;
-            controllerForce = (transform.position - lastControllerPosition) / Time.fixedDeltaTime * -forceMult;// * triggerVal;
-            controllerDistance = transform.position - centerOfMass.position;
-            controllerTorque = Vector3.Cross(controllerDistance, controllerForce/forceMult * torqueMult);
+		if (device.GetTouch (triggerButton)) {
+
+			isTriggered = true;
+
+			if (partnerPaddle == null || partnerPaddle.isTriggered) {
+
+				isSwimming = true;
+
+				// float triggerVal = Mathf.Pow(device.GetAxis(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger).x, 2f);
+
+				Vector3 globalVelocity = (transform.position - lastControllerPositionGolbal) / Time.fixedDeltaTime;
+				Vector3 localVelocity = (transform.localPosition - lastControllerPositionLocal) / Time.fixedDeltaTime;
+
+				float globalSpeed = globalVelocity.magnitude;
+				globalVelocity.Normalize ();
+
+				float localSpeed = localVelocity.magnitude;
+				localVelocity.Normalize ();
+
+				float velAdjustment = (Vector3.Dot (globalVelocity, localVelocity) + 1.3f) / 2.3f;
+
+				float finalSpeed = Mathf.Lerp (globalSpeed, localSpeed, velAdjustment);
+
+				controllerForce = globalVelocity * finalSpeed * -forceMult;// * triggerVal;
+				controllerDistance = transform.position - centerOfMass.position;
+				controllerTorque = Vector3.Cross (controllerDistance, controllerForce / forceMult * torqueMult);
 
 
-            playArea.GetComponent<Rigidbody>().AddForce(controllerForce);
-            playArea.GetComponent<Rigidbody>().AddTorque(controllerTorque);
+				playArea.AddForce (controllerForce);
+				playArea.AddTorque (controllerTorque);
+			} else {
+				isSwimming = false;
+			}
 
             
-        }
+		} else {
+			isSwimming = false;
+			isTriggered = false;
+		}
 
-        lastControllerPosition = transform.position;
+        lastControllerPositionGolbal = transform.position;
+		lastControllerPositionLocal = transform.localPosition;
 
     }
 }
